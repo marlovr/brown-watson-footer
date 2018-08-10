13767536b59a8e0059795c7aa5911ada4e71f97a
Zack Hewison <zack.s.hewison@gmail.com>
2018-08-10 23:12:45 +1000

    Initial outlook fixes

--- inline.sh
+++ inline.sh
@@ -3,6 +3,12 @@ echo "Starting inline via inliner.cm"
 rm -rf inline.html
 INLINE=$(curl 'https://inliner.cm/inline.php' 'Origin: https://inliner.cm' -H 'Content-Type: application/x-www-form-urlencoded' --silent --data-urlencode $"code=$(cat index.html)")
 echo "$INLINE" | jq -r '.HTML' > inline.html
-sed -i -e 's#src="#src="http://brown-watson.yourfilehosted.review/assets/#' inline.html
+sed -i -e 's#src="#src="http://brown-watson.yourfilehosted.review/#' inline.html
 rm -rf inline.html-e
+
+sed -i -e "s#url('#url('http://brown-watson.yourfilehosted.review/#" inline.html
+rm -rf inline.html-e
+
 echo "Inline complete"
+cat inline.html | pbcopy
+echo "Copied to clipboard"
