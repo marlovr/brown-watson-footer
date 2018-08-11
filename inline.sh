e96609639eaea4d6594f44fb904991aad84f0dec
Zack Hewison <zack.s.hewison@gmail.com>
2018-08-11 22:46:23 +1000

    Final fixes

--- inline.sh
+++ inline.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 echo "Starting inline via inliner.cm"
-rm -rf inline.html
-INLINE=$(curl 'https://inliner.cm/inline.php' 'Origin: https://inliner.cm' -H 'Content-Type: application/x-www-form-urlencoded' --silent --data-urlencode $"code=$(cat index.html)")
-echo "$INLINE" | jq -r '.HTML' > inline.html
-sed -i -e 's#src="#src="http://brown-watson.yourfilehosted.review/#' inline.html
-rm -rf inline.html-e
 
-sed -i -e "s#url('#url('http://brown-watson.yourfilehosted.review/#" inline.html
-rm -rf inline.html-e
+#cp index.html output.html
+
+rm -rf inline_outlook.html
+INLINE_OUTLOOK=$(curl 'https://inliner.cm/inline.php' 'Origin: https://inliner.cm' -H 'Content-Type: application/x-www-form-urlencoded' --silent --data-urlencode $"code=$(cat outlook.html)")
+echo "$INLINE_NOT_OUTLOOK" | jq -r '.HTML' > inline_outlook.html
+# sed -i -e 's#src="#src="http://brown-watson.yourfilehosted.review/#' inline_outlook.html
+# rm -rf inline_outlook.html-e
+
+# sed -i -e "s#url('#url('http://brown-watson.yourfilehosted.review/#" inline_outlook.html
+# rm -rf inline_outlook.html-e
+
+
+# rm -rf inline_not_outlook.html
+# INLINE_NOT_OUTLOOK=$(curl 'https://inliner.cm/inline.php' 'Origin: https://inliner.cm' -H 'Content-Type: application/x-www-form-urlencoded' --silent --data-urlencode $"code=$(cat not_outlook.html)")
+# echo "$INLINE_NOT_OUTLOOK" | jq -r '.HTML' > inline_not_outlook.html
+# sed -i -e 's#src="#src="http://brown-watson.yourfilehosted.review/#' inline_not_outlook.html
+# rm -rf inline_not_outlook.html-e
+
+# sed -i -e "s#url('#url('http://brown-watson.yourfilehosted.review/#" inline_not_outlook.html
+# rm -rf inline_not_outlook.html-e
 
 echo "Inline complete"
-cat inline.html | pbcopy
-echo "Copied to clipboard"
+#cat output.html | pbcopy
+#echo "Copied to clipboard"
